746fc90d2fd70e81c4fc47c79cce7ae266e4972e
Sid Bentifraouine <sidbentifraouine@gmail.com>
2021-08-06 06:37:48 +0200

    connect room and messages together

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :rooms
+  resources :rooms do
+    # connects messages and rooms together
+    resources :messages
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
