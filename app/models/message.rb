8119c7740af563818213dc55ad1204a9deeab5f8
Sid Bentifraouine <sidbentifraouine@gmail.com>
2021-08-07 12:23:18 +0200

    realtime update with stream/cable

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,10 @@
 class Message < ApplicationRecord
   belongs_to :room
-  after_create_commit -> { broadcast_append_to room }
-  after_destroy_commit -> { broadcast_remove_to room }
+  broadcasts_to :room
+
+  # above replaces those below
+  #
+  # after_create_commit -> { broadcast_append_to room }
+  # after_destroy_commit -> { broadcast_remove_to room }
+  # after_update_commit -> { broadcast_replace_to room }
 end
